e3399c0da6efaa43ad47daa8ff5012e35e90fc5d
Aditya Saxena <adityasaxena914@gmail.com>
2025-03-09 15:42:34 +0530

    Added C++ Competitive Programming Template

---
+++ snippets/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+
+void solve() {
+    // Your logic here
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
